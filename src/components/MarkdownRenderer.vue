b9006e49b53fd5f194fd7f384da0810b97ab57da
pmaxhogan <max@maxhogan.dev>
2023-03-11 16:34:46 -0600

    Add markdown renderer tests

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -1,5 +1,9 @@
 <script setup>
+import * as DOMPurify from "dompurify";
+import { parse } from "marked";
 import { ref, watchEffect } from "vue";
+
+const sanitize = DOMPurify.default.sanitize;
 
 const props = defineProps({
   content: {
@@ -8,12 +12,20 @@ const props = defineProps({
   },
 });
 
+const MAX_CONTENT_LENGTH = 10 * 1000 * 1000;
+if (props.content.length > MAX_CONTENT_LENGTH) {
+  throw new Error("Content too long");
+}
+
 const output = ref("");
 
 watchEffect(() => {
-  output.value = window.DOMPurify.sanitize(window.marked.parse(props.content));
+  output.value = sanitize(parse(props.content));
   window.requestAnimationFrame(() => {
-    window.MathJax.typeset();
+    if (typeof window.MathJax !== "undefined") {
+      // FIXME: testing does not work
+      window.MathJax.typeset();
+    }
   });
 });
 </script>
