59fea70f2ff7190619473b3f1e7a749b486bd7db
VTc0des <varshathomas19@gmail.com>
2023-03-20 11:21:29 -0500

    added header and footer components removed vue logo etc. from app.vue needs formatting

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const year = computed(() => new Date().getFullYear());
+    return { year };
+  },
+};
+</script>
+
+<template>
+  <footer>
+    <p>Copyright &copy; {{ year }}</p>
+  </footer>
+</template>
+
+<style>
+p {
+  text-align: center;
+}
+</style>
