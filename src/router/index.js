108b01fe80f2e4a3d638df9dc29168e06830e959
dieynababa03 <dieynaba.ba6675@gmail.com>
2023-03-24 13:15:26 -0500

    Added login to router

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const router = createRouter({
     {
       path: "/login",
       name: "login",
-      component: () => import("../views/Login.vue"),
+      component: () => import("../views/LoginView.vue"),
     },
   ],
 });
