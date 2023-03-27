9659a7eea6214f66069fa2a450a52454886c5486
Dieynaba Ba <dieynaba.ba6675@gmail.com>
2023-03-27 18:56:06 -0500

    Added Login functionality

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,33 +1,33 @@
-<script setup lang="ts">
-import { Field, Form, ErrorMessage, defineRule } from 'vee-validate';
-import * as yup from 'yup';
-const schema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(7).required(),
-});
-    function onSubmit(){
-        alert("Successfully logged in");
+<script>
+  export default{
+    data(){
+    },
+    methods:{
+      hello(){
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        if(email === '' || email.length < 10 || password === '' || password.length < 6){
+          alert("Invalid email or password");
+        }
+        else{
+          window.location.href = '/markdown';
+        }
+      },
     }
-    function onInvalid(){
-        alert("Invalid username or password");
-    }
-
+  };
 </script>
 
 <template>
-  <div>
-    <Form @submit="onSubmit" :validation-schema="schema" @invalid-submit="onInvalid">
-      <div>
-        <label for="email">Email</label>
-        <br>
-        <Field id="email" name="email" type="email" />
-        <br>
-        <label for="password">Password</label>
-        <br>
-        <Field id="password" name="password" type="password" />
-        <ErrorMessage name="password" />
-      </div>
-      <button type="submit" class="submit">Submit</button>
-    </Form>
-  </div>
+  <form action="" id="form">
+    <label for="email">Email:</label>
+    <br>
+    <input type="email" id="email" v-model="email">
+    <div class="error"></div>
+    <br>
+    <label for="password">Password:</label>
+    <br>
+    <input type="password" id="password">
+    <div class="error"></div>
+  </form>
+  <button type="submit" @click="hello">Log in</button>
 </template>
