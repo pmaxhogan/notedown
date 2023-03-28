7e002c6de65961c5e14b4f43f3ef313ac0f45eb0
VTc0des <varshathomas19@gmail.com>
2023-03-28 00:05:54 -0500

    added write to file capability

--- src/components/EditableDocument.vue
+++ src/components/EditableDocument.vue
@@ -1,66 +1,74 @@
+<script setup>
+import MarkdownRenderer from "@/components/MarkdownRenderer.vue";
+import { ref } from "vue";
+import CreateFile from "@/components/CreateFile.vue";
+
+const contentToRender = ref(String.raw``.trim());
+const documentName = ref("");
+const props = defineProps({
+  renderText: {
+    type: Boolean,
+    required: true,
+  },
+  downloadConfirm: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+
 <template>
-  <div class="editable-document">
-    <div
-      ref="editableArea"
+  <div>
+    <textarea
+      class="edit-filename"
+      type="text"
+      placeholder="Enter Document Name"
+      required="true"
+      v-model="documentName"
+    ></textarea>
+
+    <textarea
       class="editable-area"
-      contenteditable="true"
-      @input="onContentChange"
-      @blur="onBlur"
-      @focus="onFocus"
-    ></div>
+      type="text"
+      placeholder="Start Typing!"
+      v-model="contentToRender"
+    ></textarea>
+
+    <MarkdownRenderer
+      v-if="props.renderText"
+      :content="contentToRender"
+    ></MarkdownRenderer>
+
+    <CreateFile
+      :docname="documentName"
+      :textToSave="contentToRender"
+      :startDownload="downloadConfirm"
+    ></CreateFile>
   </div>
 </template>
 
-<script>
-export default {
-  name: "EditableDocument",
-
-  data() {
-    return {
-      focused: false,
-    };
-  },
-
-  computed: {
-    isFocused() {
-      return this.focused;
-    },
-  },
-
-  methods: {
-    onContentChange() {
-      const content = this.$refs.editableArea.innerHTML;
-      this.$emit("update", content);
-    },
-
-    onBlur() {
-      this.focused = false;
-    },
-
-    onFocus() {
-      this.focused = true;
-    },
-  },
-};
-</script>
-
 <style scoped>
-.editable-document {
-  position: relative;
+.edit-filename {
+  max-height: 25px;
+  padding: 0.1rem;
+  padding-left: 1rem;
+  resize: none;
 }
-
 .editable-area {
   min-height: 200px;
-  padding: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  outline: none;
   resize: vertical;
+  padding: 1rem;
+}
+.edit-filename,
+.editable-area {
+  border-radius: 4px;
+  border-color: #e2d5ec;
   overflow: auto;
   white-space: pre-wrap;
   word-wrap: break-word;
-  background-color: #FFF9FE;
-  border-color: #FFF9FE;
+  background-color: #fff9fe;
   color: #000000;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 </style>
--- src/views/MarkdownView.vue
+++ src/views/MarkdownView.vue
@@ -5,7 +5,6 @@ import EditableDocument from "@/components/EditableDocument.vue";
 import LinkGenerator from "@/components/LinkGenerator.vue";
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
-import SplitButton from "primevue/splitbutton";
 import "@/assets/themes/mytheme/theme.scss";
 
 const str = ref(
@@ -21,27 +20,19 @@ export default {
   data() {
     return {
       link: "",
-      display: false,
+      showShareDialog: false,
       copyConfirmed: false,
       showEditableDocument: false,
-      savedDocuments: [],
-      newDocumentName: "",
-      savedFolders: [
-        {
-          name: "Default",
-          documents: [],
-        },
-        {
-          name: "Trash",
-          documents: [],
-        },
-      ],
+      showHTML: false,
+      download: false,
     };
   },
   components: {
     EditableDocument,
     LinkGenerator,
+    MarkdownRenderer,
   },
+
   methods: {
     copy() {
       this.$refs.clone.focus();
@@ -52,45 +43,18 @@ export default {
       this.link = sl;
     },
     toggleDialog() {
-      this.display = !this.display;
+      this.showShareDialog = !this.showShareDialog;
       this.copyConfirmed = false;
     },
     toggleEditableDocument() {
       this.showEditableDocument = !this.showEditableDocument;
     },
-    saveDocument() {
-      if (this.newDocumentName) {
-        this.savedDocuments.push({
-          name: this.newDocumentName,
-          content: this.str.value,
-        });
-        this.newDocumentName = "";
-        this.showEditableDocument = false;
-      } else {
-        alert("Please enter a document name:");
-      }
+    toggleHTMLView() {
+      this.showHTML = !this.showHTML;
     },
-    loadDocument(index) {
-      this.str.value = this.savedDocuments[index].content;
-    },
-    createFolder(folderName) {
-      this.savedFolders.push({ name: folderName, documents: [] });
-    },
-    addDocumentToFolder(folderName, document) {
-      const folder = this.savedFolders.find((f) => f.name === folderName);
-      if (folder) {
-        folder.documents.push(document);
-      } else {
-        console.error("Folder not found:", folderName);
-      }
-    },
-    getDocumentsInFolder(folderName) {
-      const folder = this.savedFolders.find((f) => f.name === folderName);
-      return folder ? folder.documents : [];
-    },
-    isValidDocumentName(name) {
-      return /^[a-zA-Z0-9]/.test(name);
-    },
+    initiateDownload() {
+      this.download = !this.download;
+    }
   },
 };
 </script>
@@ -100,99 +64,65 @@ export default {
     <textarea v-model="str"></textarea>
     <MarkdownRenderer :content="str"></MarkdownRenderer>
 
-    <div class="position-sticky mx-auto my-auto">
+    <div>
+      <EditableDocument
+        v-if="showEditableDocument"
+        :renderText="showHTML"
+        :downloadConfirm="download"
+      ></EditableDocument>
+
       <span class="p-buttonset">
         <Button
-          class="mr-8"
           @click="toggleEditableDocument"
-          label="Create/Edit Document"
+          label="Create"
           icon="pi pi-file"
         ></Button>
         <Button
-          @click="saveDocument"
-          label="Save Document"
-          icon="pi pi-save"
+          @click="toggleHTMLView"
+          label="Preview"
+          icon="pi pi-eye"
         ></Button>
-        <SplitButton
-          @click="
-            addDocumentToFolder(selectedFolder, {
-              name: newDocumentName,
-              content: str.value,
-            })
-          "
-          label="Add to Folder"
-          icon="pi pi-folder-open"
-          :model="savedFolders"
-        ></SplitButton>
+        <Button
+          @click="initiateDownload"
+          label="Download"
+          icon="pi pi-download"
+        ></Button>
+        <Button
+          @click="toggleDialog"
+          label="Share"
+          icon="pi pi-external-link"
+        ></Button>
       </span>
     </div>
-
-    <div v-if="showEditableDocument">
-      <textarea
-        type="text"
-        v-model="newDocumentName"
-        placeholder="Document name"
-      ></textarea>
-      <EditableDocument :content="str" @update="onDocumentUpdate" />
-      <select v-model="selectedFolder">
-        <option
-          v-for="(folder, index) in savedFolders"
-          :key="index"
-          :value="folder.name"
-        >
-          {{ folder.name }}
-        </option>
-      </select>
-    </div>
-
-    <h2>Folders</h2>
-    <ul>
-      <li v-for="(folder, index) in savedFolders" :key="index">
-        {{ folder.name }}
-      </li>
-    </ul>
-
-    <h2>Saved Documents</h2>
-    <ul>
-      <li v-for="(document, index) in savedDocuments" :key="index">
-        <a href="#" @click.prevent="loadDocument(index)">{{ document.name }}</a>
-      </li>
-    </ul>
   </div>
 
-  <div class="position-sticky bottom-0 end-0">
-    <Button
-      @click="toggleDialog"
-      label="Share"
-      icon="pi pi-external-link"
-    ></Button>
+  <Dialog position="center" v-model:visible="showShareDialog">
+    <template #header>
+      <h3 v-if="copyConfirmed">Link Copied! Click to Close</h3>
+      <h3 v-else>Invite Others to View Your NoteDown</h3>
+    </template>
 
-    <LinkGenerator @shareableLink="captureLink" />
+    <LinkGenerator @shareableLink="captureLink"></LinkGenerator>
 
-    <Dialog position="center" v-model:visible="display">
-      <template #header>
-        <h3 v-if="copyConfirmed">Link Copied! Click to Close</h3>
-        <h3 v-else>Invite Others to View Your NoteDown</h3>
-      </template>
-      <textarea
-        v-if="!copyConfirmed"
-        class="linkdisplay"
-        v-on:focus="$event.target.select()"
-        ref="clone"
-        readonly
-        :value="link"
-      ></textarea>
-      <template #footer>
-        <Button
-          v-if="copyConfirmed"
-          @click="toggleDialog"
-          label="Close"
-          icon="pi pi-times"
-        ></Button>
-        <Button v-else @click="copy" label="Copy" icon="pi pi-link"></Button>
-      </template>
-    </Dialog>
-  </div>
+    <textarea
+      v-if="!copyConfirmed"
+      class="displayLinkArea"
+      v-on:focus="$event.target.select()"
+      ref="clone"
+      readonly
+      :value="link"
+    ></textarea>
+
+    <template #footer>
+      <Button
+        v-if="copyConfirmed"
+        @click="toggleDialog"
+        label="Close"
+        icon="pi pi-times"
+      ></Button>
+      <Button v-else @click="copy" label="Copy" icon="pi pi-link"></Button>
+    </template>
+  </Dialog>
 </template>
 
 <style lang="scss">
@@ -202,7 +132,7 @@ textarea {
   background-color: #fff9fe;
   border-color: #fff9fe;
 }
-.linkdisplay {
+.displayLinkArea {
   width: 100%;
   height: 25px;
   border-radius: 5px;
@@ -211,5 +141,3 @@ textarea {
   border-color: #fff9fe;
 }
 </style>
-
-
