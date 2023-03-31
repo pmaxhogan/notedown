b03e6dfc40c99e4d5fe5db7ff53e9f34148e84ff
Habiba Salman <habiba.salman2002@gmail.com>
2023-03-30 20:46:39 -0500

    added DeleteButton

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="delete-button" @click="deleteNote">
+      <i class="fas fa-trash"></i> Delete
+    </button>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    props: {
+      noteId: {
+        type: Number,
+        required: true
+      }
+    },
+    methods: {
+      deleteNote() {
+        this.$emit('delete-note', this.noteId);
+        return [];
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .delete-button {
+    background-color: #7519b3;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    cursor: pointer;
+  }
+  </style>
+  
+  
