98d3d9876fdc7324fc15f312be84dce1c199f952
VTc0des <varshathomas19@gmail.com>
2023-04-11 17:42:39 -0500

    removed unused vars

--- src/lib/getShareableLink.js
+++ src/lib/getShareableLink.js
@@ -1,9 +1,6 @@
 import { getAuth } from "firebase/auth";
 
 export default async function getShareableLink(documentID) {
-  //const userID = ref("");
-  //const docRef = ref("");
-  //getDoc(docRef);
   const currentUid = (await getAuth()).currentUser.uid;
   const path = `/viewdoc/${currentUid}/${documentID}`;
   return new URL(path, location.href).href;
